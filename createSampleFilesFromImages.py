a2bce42ddd65ddca4bd5b15e6661bf015b05cd38
Myeong <maumtech@yahoo.com>
2020-03-19 17:38:34 -0400

    Move the util functions to util.py

--- createSampleFilesFromImages.py
+++ createSampleFilesFromImages.py
@@ -10,22 +10,7 @@ import numpy as np
 
 blankHash = "bf619eac0cdf3f68d496ea9344137e8b"
 
-def getSize(fileobject):
-    fileobject.seek(0,2) # move the cursor to the end of the file
-    size = fileobject.tell()
-    return size
 
-
-def printTimeWithInfoString(infoString) :
-    timeForDisplay = time.strftime("%c")
-    print(timeForDisplay + ': ' + infoString)
-
-def creatDirIfNotExist(dirName):
-   if not os.path.exists(dirName):
-       os.mkdir(dirName)
-       print("Directory " , dirName ,  " Created ")
-   else:
-       print("Directory " , dirName ,  " already exists")
 
 def createSampleFiles(osRemove):
 
